3d53cb7d66a7d8fc5009be8a6e27345c860f737b
Dirk Arnez <smalldirkalex@gmail.com>
2024-08-23 12:58:16 +0800

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,4 +1,3 @@
 start "" .\installer.exe &&^
 C:\Windows\System32\cmd.exe /c start /wait /min C:\Windows\System32\timeout.exe /t 5 &&^
 (C:\Windows\System32\taskkill.exe /f /im installer.exe && C:\Windows\System32\taskkill.exe /f /im streamer.exe)
-pause
